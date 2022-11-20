342e3631bf07a0dc7704143490b5bd20ae807d0a
Marc-Antoine <magiguere@mirego.com>
2022-11-20 11:39:09 -0500

    implement paginator

--- src/views/table-helpers.go
+++ src/views/table-helpers.go
@@ -2,15 +2,15 @@ package views
 
 import (
 	"math"
-	"os"
 
 	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/paginator"
 	"github.com/charmbracelet/bubbles/table"
 	"github.com/charmbracelet/lipgloss"
 	"golang.org/x/crypto/ssh/terminal"
 )
 
-const horizontalPadding = 10
+const horizontalPadding = 12
 
 func GetTableStyles() table.Styles {
 	s := table.DefaultStyles()
@@ -83,7 +83,7 @@ func createColSelectionRows(data map[string]string) []table.Row {
 
 // fun getScreenSize return a tuple containing the width and the height respectively.
 func getScreenSize() (int, int) {
-	width, height, err := terminal.GetSize(int(os.Stdin.Fd()))
+	width, height, err := terminal.GetSize(0)
 	if err != nil {
 		panic(err)
 	}
@@ -96,28 +96,95 @@ func createTable(cols []table.Column, rows []table.Row, mode TableMode) TableMod
 
 	t := table.New(
 		table.WithKeyMap(table.KeyMap{
-			LineUp:       createBinding("w", "up"),
-			LineDown:     createBinding("s", "down"),
-			PageUp:       createBinding("W"),
-			PageDown:     createBinding("S"),
-			HalfPageUp:   createBinding("a"),
-			HalfPageDown: createBinding("d"),
-			GotoTop:      createBinding("g"),
-			GotoBottom:   createBinding("G"),
+			LineUp:     createBinding("w", "up"),
+			LineDown:   createBinding("s", "down"),
+			PageUp:     createBinding("W"),
+			PageDown:   createBinding("S"),
+			GotoTop:    createBinding("g"),
+			GotoBottom: createBinding("G"),
 		}),
 		table.WithColumns(cols),
-		table.WithRows(rows),
+		table.WithRows(rows[:100]),
 		table.WithFocused(true),
 		table.WithWidth(w-4),
 		table.WithHeight(h-10),
 	)
-
 	t.SetStyles(GetTableStyles())
 
-	return TableModel{Table: t, Mode: mode}
+	p := paginator.New()
+	p.PerPage = 100
+	p.SetTotalPages(len(rows))
+
+	return NewTableModel(t, p, mode, rows)
 }
 
 func createBinding(binding ...string) key.Binding {
 	b := key.NewBinding(func(b *key.Binding) { b.SetKeys(binding...) })
 	return b
+}
+
+func (m *TableModel) showInfo(message string) {
+	m.showMessage(message, &infoMessageStyle)
+}
+
+func (m *TableModel) showWarning(message string) {
+	m.showMessage(message, &warningMessageStyle)
+}
+
+func (m *TableModel) showError(message string) {
+	m.showMessage(message, &errorMessageStyle)
+}
+
+func (m *TableModel) showMessage(message string, style *lipgloss.Style) {
+	finalMessage := message
+	if style != nil {
+		finalMessage = style.Render(message)
+	}
+
+	m.bottomMessage = finalMessage
+}
+
+func removeIntFromSlice(slice []int, valueToRemove int) []int {
+	if len(slice) == 0 {
+		return slice
+	}
+
+	var index int = -1
+	for i, v := range slice {
+		if v == valueToRemove {
+			index = i
+			break
+		}
+	}
+
+	if index == -1 {
+		return slice
+	} else if len(slice) <= index+1 {
+		return slice[:index]
+	}
+
+	return append(slice[:index], slice[index+1:]...)
+}
+
+func (m *TableModel) getSelectedRowKeys() []string {
+	rows := m.table.Rows()
+
+	keys := []string{}
+	for _, index := range m.selectedRows {
+		keys = append(keys, rows[index][1])
+	}
+
+	return keys
+}
+
+func (m *TableModel) changePage(incrementPage bool, n int) {
+	for i := 0; i < n; i++ {
+		if incrementPage {
+			m.paginator.NextPage()
+		} else {
+			m.paginator.PrevPage()
+		}
+	}
+	start, end := m.paginator.GetSliceBounds(len(m.allRows))
+	m.table.SetRows(m.allRows[start:end])
 }
--- src/views/table.go
+++ src/views/table.go
@@ -2,39 +2,41 @@ package views
 
 import (
 	"fmt"
-	"planetary-observation/data"
+	"strings"
 
+	"github.com/charmbracelet/bubbles/paginator"
 	"github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 )
 
 type TableMode string
-
 type TableModel struct {
-	Table         table.Model
-	Mode          TableMode
+	table         table.Model
+	mode          TableMode
 	selectedRows  []int
 	bottomMessage string
 	csvTable      *TableModel
+	paginator     paginator.Model
+	allRows       []table.Row
 }
 
-const maxColN = 10
+const maxColN = 8
 const (
 	CSV       = "csv"
 	ColSelect = "colSelect"
 )
 
-var baseStyle = lipgloss.NewStyle().
-	BorderStyle(lipgloss.DoubleBorder()).
-	BorderForeground(lipgloss.Color("240"))
-
-var infoMessageStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#489eff"))
-var warningMessageStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#ffed68"))
-var errorMessageStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#f94848"))
+func NewTableModel(t table.Model, p paginator.Model, mode TableMode, items []table.Row) TableModel {
+	return TableModel{
+		table:     t,
+		mode:      mode,
+		paginator: p,
+		allRows:   items,
+	}
+}
 
 func (m TableModel) Init() tea.Cmd {
-	if m.Mode == ColSelect {
+	if m.mode == ColSelect {
 		m.selectedRows = []int{}
 		m.csvTable = nil
 	}
@@ -47,120 +49,58 @@ func (m TableModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "esc":
-			if m.Table.Focused() {
-				m.Table.Blur()
+			if m.table.Focused() {
+				m.table.Blur()
 			} else {
-				m.Table.Focus()
+				m.table.Focus()
 			}
 		case "q", "ctrl+c":
 			return m, tea.Quit
 		case "f":
-			if m.Mode == ColSelect {
-				if len(m.selectedRows) > 0 {
-					csvTable := CreateTableForCSVData(
-						m.getSelectedRowKeys(),
-						data.FilterCSVColumns(data.LoadPlanetarySystems(false)[:100], m.selectedRows),
-					)
-					m.csvTable = &csvTable
-					return m.csvTable.Update(nil)
-				} else {
-					m.showWarning("You must select at least one column to display.")
-				}
+			if m.mode == ColSelect {
+				return handleCsvTableGeneration(&m)
 			}
 		case "enter", "e":
-			if m.Mode == ColSelect {
-				if len(m.selectedRows) < maxColN {
-					selectedRow := m.Table.SelectedRow()
-					if selectedRow[0] == "[ ]" {
-						selectedRow[0] = "[x]"
-						m.selectedRows = append(m.selectedRows, m.Table.Cursor())
-					} else {
-						selectedRow[0] = "[ ]"
-						m.selectedRows = removeIntFromSlice(m.selectedRows, m.Table.Cursor())
-					}
+			if m.mode == ColSelect {
+				handleColSelection(&m)
+			}
 
-					rows := m.Table.Rows()
-					rows[m.Table.Cursor()] = selectedRow
-					m.Table.SetRows(rows)
-					m.showInfo(fmt.Sprintf("%d/%d row selected", len(m.selectedRows), maxColN))
-				} else {
-					m.showWarning(
-						fmt.Sprintf(
-							"%d/%d row selected. You have already selected the maximum number of rows.",
-							len(m.selectedRows),
-							maxColN,
-						),
-					)
-				}
+		case "d":
+			if m.mode == CSV {
+				m.changePage(true, 1)
+			}
+
+		case "a":
+			if m.mode == CSV {
+				m.changePage(false, 1)
+			}
+
+		case "D":
+			if m.mode == CSV {
+				m.changePage(true, 10)
+			}
+
+		case "A":
+			if m.mode == CSV {
+				m.changePage(false, 10)
 			}
 
 		case "p":
-			if m.Mode == ColSelect {
+			if m.mode == ColSelect {
 				m.showInfo(fmt.Sprintf("selected rows: %v", m.selectedRows))
 			}
 		}
 	}
-	m.Table, cmd = m.Table.Update(msg)
+	m.table, cmd = m.table.Update(msg)
 	return m, cmd
 }
 
 func (m TableModel) View() string {
-	if m.csvTable != nil {
-		return m.csvTable.View()
-	}
-	return baseStyle.Render(m.Table.View()) + "\n" + m.bottomMessage
-}
+	var sb strings.Builder
 
-func (m *TableModel) showInfo(message string) {
-	m.showMessage(message, &infoMessageStyle)
-}
-
-func (m *TableModel) showWarning(message string) {
-	m.showMessage(message, &warningMessageStyle)
-}
-
-func (m *TableModel) showError(message string) {
-	m.showMessage(message, &errorMessageStyle)
-}
-
-func (m *TableModel) showMessage(message string, style *lipgloss.Style) {
-	finalMessage := message
-	if style != nil {
-		finalMessage = style.Render(message)
-	}
-
-	m.bottomMessage = finalMessage
-}
-
-func removeIntFromSlice(slice []int, valueToRemove int) []int {
-	if len(slice) == 0 {
-		return slice
-	}
-
-	var index int = -1
-	for i, v := range slice {
-		if v == valueToRemove {
-			index = i
-			break
-		}
-	}
-
-	if index == -1 {
-		return slice
-	} else if len(slice) <= index+1 {
-		return slice[:index]
-	}
-
-	return append(slice[:index], slice[index+1:]...)
-}
-
-func (m *TableModel) getSelectedRowKeys() []string {
-	rows := m.Table.Rows()
-
-	keys := []string{}
-	for _, index := range m.selectedRows {
-		keys = append(keys, rows[index][1])
-	}
-
-	return keys
+	sb.WriteString(baseStyle.Render(m.table.View()) + "\n")
+	sb.WriteString(m.bottomMessage + "\n")
+	sb.WriteString(fmt.Sprintf("Col: %d/%d\n", m.table.Cursor()+1, len(m.table.Rows())))
+	sb.WriteString(fmt.Sprintf("Page: %s\n", m.paginator.View()))
+	return sb.String()
 }
